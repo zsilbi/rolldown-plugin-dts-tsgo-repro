6a016b54ed6813ee063ae770a1f8c341d7bf35ba
Balázs Németh <zsilbi@zsilbi.hu>
2025-06-07 17:54:27 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## export \* from 'externalized-dep' generates unnecessary code
+
+Related: https://github.com/rolldown/rolldown/issues/1940
---
+++ packages/my-package/rolldown.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "rolldown";
+import { dts } from "rolldown-plugin-dts";
+
+export default defineConfig({
+  input: ["./src/index.js"],
+  plugins: [dts({ tsgo: true })],
+});
